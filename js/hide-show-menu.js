22f072e356c2834662fd01dc09cfca6e6e4733ef
st_Vader <st.vader.85@gmail.com>
2017-04-29 22:07:27 +0300

    complete modal window for gallery, some style fix foe all project

--- js/hide-show-menu.js
+++ js/hide-show-menu.js
@@ -1,22 +1,23 @@
 function changeMenuPoint(classBlock, classItem, additionClass) {
 	var block = document.querySelector('.' + classBlock);
 	var items = block.querySelectorAll('.' + classItem);
-	
+
 
 	function changeItem(event) {
+
 		var target = event.target;
-		var targetParent = target.parent;
+		var targetParent = target.parentNode;
 		var i;
 		var itemParent;
 
 		if (target.tagName != 'A' 
-			&& !target.classList.contains(classItem)
-			&& targetParent.classList.contains(additionClass)) return;
+			|| !target.classList.contains(classItem)
+			|| targetParent.classList.contains(additionClass)) return;
 
-		event.//delete default
-
+		event.preventDefault();
+		
 		for (i=0; i<items.length; i++) {
-			itemParent = items[i].parent;
+			itemParent = items[i].parentNode;			
 			if (itemParent.classList.contains(additionClass)) {
 				itemParent.classList.remove(additionClass);
 			}
